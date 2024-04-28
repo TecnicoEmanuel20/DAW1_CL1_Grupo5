f35a5388ee1b36080bcc8bf084f64ce2a7bbc694
TecnicoEmanuel20 <perezanchayaedgaremanuel@gmail.com>
2024-04-28 17:56:04 -0500

    Iniciando Examen CL1 EDGAR

---
+++ src/main/java/com/cl1/DAWI_CL1_Grupo5_EdgarPerez/DawiCl1Grupo5EdgarPerezApplication.java
@@ -0,0 +1,13 @@
+package com.cl1.DAWI_CL1_Grupo5_EdgarPerez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl1Grupo5EdgarPerezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl1Grupo5EdgarPerezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cl1/DAWI_CL1_Grupo5_EdgarPerez/DawiCl1Grupo5EdgarPerezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cl1.DAWI_CL1_Grupo5_EdgarPerez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl1Grupo5EdgarPerezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
